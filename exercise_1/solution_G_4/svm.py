1fe17abd0ca52faabfb9c7eb8a25debdeb0c5931
Jan Leszczyk <leszczyk.jan@gmail.com>
2024-11-25 12:48:30 +0100

    added SVM solution

--- exercise_1/solution_G_4/svm.py
+++ exercise_1/solution_G_4/svm.py
@@ -43,7 +43,9 @@ class SVM:
             numpy.ndarray: A matrix of shape (n_samples_1, n_samples_2) representing the linear kernel between x1 and x2.
         """
         # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
-        return
+        # Linear kernel means the inner products of the samples are the output.
+        # Therefore, x2 has to be transposed in order to get the n_sample x n_sample matrix.
+        return np.dot(x1,x2.T)
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
 
     @staticmethod
@@ -60,7 +62,13 @@ class SVM:
         """
 
         # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
-        return
+        # RBF kernel means using the formula
+        #           ||x1 - x2||²          
+        #         - -----------
+        #   K = e^   2*sigma²
+        diff = x1[:, None,:] - x2[None,:,:]     # subtract each element of each vector from each other
+        dist = np.linalg.norm(diff, axis=-1)**2 # norm the difference -> distance
+        return np.exp(-dist / (2*(sigma**2)))
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
 
     def fit(self, X, y):
@@ -93,12 +101,12 @@ class SVM:
 
         # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
         # SEE: https://cvxopt.org/examples/tutorial/qp.html and https://cvxopt.org/userguide/coneprog.html#quadratic-programming and http://www.seas.ucla.edu/~vandenbe/publications/mlbook.pdf
-        Q = cvxopt.matrix() # to be corrected by you
-        p = cvxopt.matrix() # to be corrected by you
-        G = cvxopt.matrix() # to be corrected by you
-        h = cvxopt.matrix() # to be corrected by you
-        A = cvxopt.matrix() # to be corrected by you
-        b = cvxopt.matrix() # to be corrected by you
+        Q = cvxopt.matrix(np.outer(y,y) * K)
+        p = cvxopt.matrix(-1 * np.ones(n_observations))
+        G = cvxopt.matrix(-1 * np.eye(n_observations))
+        h = cvxopt.matrix(np.zeros((n_observations)))
+        A = cvxopt.matrix(y)
+        b = cvxopt.matrix(0.0)
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
 
         cvxopt.solvers.options["show_progress"] = False
@@ -106,8 +114,10 @@ class SVM:
 
         # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
         # Save the solution
-        self.__alphas = None
-        self.__bias = None
+        self.__alphas = np.ravel(solution['x'])
+        sv_ind = np.where(self.__alphas > 1e-5)[0]  # Look for support vector indices
+        y_sv = y[0][sv_ind]                         # y-values for support vectors
+        self.__bias = np.mean(y_sv - np.sum((y*self.__alphas)*K[:][sv_ind], axis=1))
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
 
         self.__targets = y
@@ -125,9 +135,11 @@ class SVM:
 
         X = X / self.__norm
 
-        # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****
+        # *****BEGINNING OF YOUR CODE (DO NOT DELETE THIS LINE)*****     
         if self.__kernel == SVM.linear_kernel:
-            return 
+            K = self.__kernel(X, self.__training_X)
+            return np.sign(np.sum(((self.__alphas * self.__targets)*K), axis=1) + self.__bias)
         elif self.__kernel == SVM.rbf_kernel:
-            return 
+            K = self.__kernel(X, self.__training_X, self.__sigma)
+            return np.sign(np.sum(((self.__alphas * self.__targets)*K), axis=1) + self.__bias)
         # *****END OF YOUR CODE (DO NOT DELETE THIS LINE)*****
